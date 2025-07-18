bb3cf8146659ea40d29a739ed603b5379f5949d6
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-07-18 13:43:49 +0200

    feat: complete milestone 3

---
+++ src/components/TaskRow.jsx
@@ -0,0 +1,15 @@
+import { memo } from 'react';
+
+export default memo(function TaskRow({ data }) {
+    const { title, status, createdAt } = data;
+
+    return (
+        <>
+            <tr className='task-row'>
+                <td>{title}</td>
+                <td className={`${status === 'To do' ? 'to-do' : status === 'Doing' ? 'doing' : 'done'}`}>{status}</td>
+                <td>{createdAt}</td>
+            </tr>
+        </>
+    );
+}); 
--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -1,5 +1,21 @@
+import TaskRow from '../components/TaskRow';
+import { useTask } from '../contexts/GlobalContext';
+
 export default function TaskList() {
+
+    const { tasks } = useTask();
     return (
-        <></>
+        <table className='task-table'>
+            <thead>
+                <tr className='task-header'>
+                    <th>Nome</th>
+                    <th>Stato</th>
+                    <th>Data di Creazione</th>
+                </tr>
+            </thead>
+            <tbody>
+                {tasks.map(task => (< TaskRow key={task.id} data={task} />))}
+            </tbody>
+        </table>
     );
 }
