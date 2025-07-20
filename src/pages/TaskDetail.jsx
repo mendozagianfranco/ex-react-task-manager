71bcd591752013832b1562d8fa725bdfa7165900
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-07-20 16:54:51 +0200

    fix(TaskDetail): added fallback UI when task is not found

--- src/pages/TaskDetail.jsx
+++ src/pages/TaskDetail.jsx
@@ -5,14 +5,20 @@ import { useContextTasks } from '../contexts/GlobalContext';
 export default function TaskDetail() {
     const { id } = useParams();
     const { tasks } = useContextTasks();
-    const { title, description, status, createdAt } = tasks.find(task => task.id === Number(id));
+    const task = tasks.find(task => task.id === Number(id));
+
+    if (!task) {
+        return (
+            <h1>Task non trovata</h1>
+        );
+    }
 
     return (
         <>
-            <h1>Titolo: {title}</h1>
-            <p>Descrizione: {description}</p>
-            <p>Stato: {status}</p>
-            <p>Data di creazione: {new Date(createdAt).toLocaleDateString()}</p>
+            <h1>Titolo: {task.title}</h1>
+            <p>Descrizione: {task.description}</p>
+            <p>Stato: {task.status}</p>
+            <p>Data di creazione: {new Date(task.createdAt).toLocaleDateString()}</p>
             <button onClick={() => console.log('Elimino Task')}>Elimina Task</button>
         </>
     );
