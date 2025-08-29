ad170c0213f14f1084980d80d4c50697fa732047
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-08-29 15:03:10 +0200

    feat: complete milestone 12

--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -1,12 +1,25 @@
-import { useMemo, useState } from 'react';
+import { useCallback, useMemo, useRef, useState } from 'react';
 import TaskRow from '../components/TaskRow';
 import { useContextTasks } from '../contexts/GlobalContext';
+
+function debounce(callback, delay) {
+    let timer;
+    return (value) => {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            callback(value);
+        }, delay);
+    };
+}
 
 export default function TaskList() {
 
     const { tasks } = useContextTasks();
     const [sortBy, setSortBy] = useState('createdAt');
     const [sortOrder, setSortOrder] = useState(1);
+    const [searchQuery, setSearchQuery] = useState('');
+
+    const debouncedSetSearchQuery = useCallback(debounce(setSearchQuery, 500), []);
 
     function handleSort(field) {
         if (sortBy === field) {
@@ -17,8 +30,10 @@ export default function TaskList() {
         }
     }
 
-    const sortedTask = useMemo(() => {
-        return [...tasks].sort((a, b) => {
+    const filteredAndSortedTaks = useMemo(() => {
+        const filteredTask = [...tasks].filter(t => t.title.toLowerCase().includes(searchQuery.toLowerCase()));
+
+        return filteredTask.sort((a, b) => {
             let comparison;
 
             if (sortBy == 'title') {
@@ -34,21 +49,28 @@ export default function TaskList() {
 
             return comparison * sortOrder;
         });
-    }, [tasks, sortBy, sortOrder]);
+    }, [tasks, sortBy, sortOrder, searchQuery]);
 
 
     return (
-        <table className='task-table'>
-            <thead>
-                <tr className='task-header'>
-                    <th onClick={() => handleSort('title')}>Nome</th>
-                    <th onClick={() => handleSort('status')}>Stato</th>
-                    <th onClick={() => handleSort('createdAt')}>Data di Creazione</th>
-                </tr>
-            </thead>
-            <tbody>
-                {sortedTask.map(task => (< TaskRow key={task.id} data={task} />))}
-            </tbody>
-        </table>
+        <>
+            <br />
+            <label>
+                Ricerca
+                <input type="text" onChange={(e) => debouncedSetSearchQuery(e.target.value)} />
+            </label>
+            <table className='task-table'>
+                <thead>
+                    <tr className='task-header'>
+                        <th onClick={() => handleSort('title')}>Nome</th>
+                        <th onClick={() => handleSort('status')}>Stato</th>
+                        <th onClick={() => handleSort('createdAt')}>Data di Creazione</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {filteredAndSortedTaks.map(task => (< TaskRow key={task.id} data={task} />))}
+                </tbody>
+            </table>
+        </>
     );
 }
